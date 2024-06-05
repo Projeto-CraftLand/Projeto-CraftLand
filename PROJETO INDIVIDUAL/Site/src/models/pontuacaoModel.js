041656110ccefb4e3b08d948375343c7b4ee8442
GustavoBasi <gustavo.bcruz@sptech.school>
2024-06-05 11:56:24 -0300

    Projeto CraftLand

--- PROJETO INDIVIDUAL/Site/src/models/pontuacaoModel.js
+++ PROJETO INDIVIDUAL/Site/src/models/pontuacaoModel.js
@@ -3,10 +3,11 @@ var database = require("../database/config");
 function buscarUltimasMedidas(idUsuario) {
 
     var instrucaoSql = `SELECT 
-        fase1 as fase1,
-        fase2 as fase2,
-        total as total
-                    FROM pontuacao
+    fase1 as fase1,
+    fase2 as fase2,
+    total as total,
+    (select round(avg(total),2) from pontuacao)media
+                FROM pontuacao
                     WHERE fkUsuario = ${idUsuario}
                     ORDER BY id DESC LIMIT 1 `;
 
